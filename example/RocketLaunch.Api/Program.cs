3f56120fa8627c5a63abb3fd1c420aafaaf2b53d
Oliver Grau <oliver.grau@grausoft.net>
2025-07-08 19:00:21 +0200

    Add Swagger and config support

--- example/RocketLaunch.Api/Program.cs
+++ example/RocketLaunch.Api/Program.cs
@@ -8,6 +8,8 @@ using DDD.BuildingBlocks.DevelopmentPackage.Storage;
 using DDD.BuildingBlocks.DI.Extensions;
 using DDD.BuildingBlocks.Hosting.Background;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using RocketLaunch.Api;
 using RocketLaunch.Application;
 using RocketLaunch.Api.Handler;
 using RocketLaunch.Domain.Service;
@@ -15,12 +17,21 @@ using RocketLaunch.ReadModel.Core.Builder;
 using RocketLaunch.ReadModel.Core.Service;
 using RocketLaunch.ReadModel.InMemory.Service;
 
-var workerId = "rocket-launch-worker";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+var rocketOptions = builder.Configuration.GetSection("RocketLaunch").Get<RocketLaunchApiOptions>() ?? new RocketLaunchApiOptions();
+var workerId = rocketOptions.WorkerId;
 
 builder.Logging.AddConsole();
 
 var services = builder.Services;
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rocket Launch API", Version = "v1" });
+});
+services.Configure<RocketLaunchApiOptions>(builder.Configuration.GetSection("RocketLaunch"));
 
 // Event publishing infrastructure
 services.AddSingleton<EventPublishingTable>(x =>
@@ -33,7 +44,7 @@ services.AddSingleton<EventPublishingTable>(x =>
 
 services.AddSingleton<DomainEventNotifier>(sp =>
 {
-    var notifier = new DomainEventNotifier("RocketLaunch.ReadModel.Core");
+    var notifier = new DomainEventNotifier(rocketOptions.ReadModelAssemblyName);
     notifier.SetDependencyResolver(new ServiceLocator(sp));
     return notifier;
 });
@@ -48,7 +59,8 @@ services.AddSingleton<DomainEventProjectionDispatcher>(sp =>
         workerId,
         sp.GetService<ILoggerFactory>()));
 
-services.Configure<TimedHostedServiceOptions>(o => o.GlobalTriggerTimeoutInMilliseconds = 1000);
+services.Configure<TimedHostedServiceOptions>(o =>
+    o.GlobalTriggerTimeoutInMilliseconds = rocketOptions.GlobalTriggerTimeoutInMilliseconds);
 
 services.AddHostedService(sp =>
     new TimedHostedService<DomainEventProjectionDispatcher>(
@@ -60,8 +72,13 @@ services.AddHostedService(sp =>
 services.AddSingleton<IEventStorageProvider>(sp =>
     new PureInMemoryEventStorageProvider(sp.GetRequiredService<EventPublishingTable>()));
 
-var snapshotPath = Path.Combine(AppContext.BaseDirectory, "mission.snapshots.dump");
-services.AddSingleton<ISnapshotStorageProvider>(sp => new InMemorySnapshotStorageProvider(5, snapshotPath));
+var snapshotPath = rocketOptions.SnapshotPath;
+if (!Path.IsPathRooted(snapshotPath))
+{
+    snapshotPath = Path.Combine(AppContext.BaseDirectory, snapshotPath);
+}
+services.AddSingleton<ISnapshotStorageProvider>(sp =>
+    new InMemorySnapshotStorageProvider(rocketOptions.SnapshotThreshold, snapshotPath));
 
 services.AddSingleton<IEventSourcingRepository>(sp =>
     new EventSourcingRepository(
@@ -88,6 +105,9 @@ services.AddSingleton<IDomainEntry>(sp => new DomainEntry(
     sp.GetRequiredService<IResourceAvailabilityService>()));
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Minimal API endpoints
 app.MapMissionRoutes();
